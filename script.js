bbc34ec3bb33063e78bb4bfd95c78c820fbfe151
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-02-26 22:34:24 +0500

    MY Commit

---
+++ script.js
@@ -0,0 +1,120 @@
+function myData(a,b , c ,d){
+    console.log(myData.length);
+}
+
+myData();
+
+const myArr = ["Karachi", "Lahore" ,"Hyderabad", "Multan" ,"Peshawar"]
+let myArr2  = myArr.slice(2);
+
+console.log(myArr2);
+
+var name = "John Appleseed"
+myName = name.slice(5,10)
+console.log(myName)
+
+var sum = 14;
+if (sum<20)!
+    alert("under");
+    else {
+        alert("over");
+    }
+
+function myfunc() {return Boolean (10>9);}
+myfunc();
+
+
+var myString = "Hamiz"
+var modString = myString.toUpperCase()
+console.log(modString)
+
+let myValue = document.getElementById("p.name").value;
+console.log(myValue)
+
+let myValue1 = document.login.uname.value;
+console.log(myValue1)
+
+var myInt = 7.25;
+var myInt1 = Math.round(myInt);
+console.log(myInt1)
+
+let myValue2 = document.getElementById("name").value;
+console.log(myValue)
+
+/* Hamiz Muzaffer 
+is very nice person */
+
+var arr = [];
+ arr[0] = "a";
+ arr[1] = "b";
+ alert(arr.length)
+
+
+function x (){
+    document.write(2+5+"8")
+}
+
+x();
+
+
+const arr = [2,7,10];
+console.log(arr[1])
+
+var  a = 0 ;
+var b = 0 ;
+while (a<3){
+    a++;
+    b+=1;
+    console.log(b);
+}
+
+var a = 7;
+
+function hello (){
+    if(true){
+        var a = 5; 
+    } alert(a);
+}
+
+hello()
+
+
+
+
+var myarray = ["foo", "bar" , "baz"];
+
+myarray.length = 0;
+myarray.push("bin");
+console.log(myarray)
+
+
+
+
+
+function func1(a, b, c) {
+    console.log(args.length);
+  }
+func1(3,4,5)
+  
+
+function printArray(a){
+  var len = a.lemgth;
+  i = 0;
+  if(len=0){
+    console.log("Empty Array")
+  }
+  else{
+    do{
+      console.log(a[i]);
+
+    }
+    while (++i, i<lem)
+  }
+}
+printArray();
+
+function foo(a,b){
+  alert(b);
+}
+
+foo(1);
